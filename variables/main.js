b185476521d031c9090893b2d58f36c5a8a403d3
maximle <mletunovskiy@yandex.ru>
2019-05-22 22:21:57 +0300

    first commit variables

---
+++ variables/main.js
@@ -0,0 +1,19 @@
+// Задание 1
+let algebra = 5;
+let geography = 4;
+let physics = 4
+
+let avgGrade = (algebra + geography + physics) / 3;
+console.log(avgGrade);
+
+// Задание 2
+let name = 'Максим';
+let message = `Привет, мир! Меня зовут ${name}`;
+console.log(message);
+
+// Задание 3
+let x = 2;
+let y = 22;
+let z = 0;
+result = x * y + 5 * z + x - 1;
+console.log(result);
