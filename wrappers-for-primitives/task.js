7fb5cd0632163800925bf79382fda2de2553e66f
maximle <mletunovskiy@yandex.ru>
2019-06-02 23:44:54 +0300

    task.js modified

--- wrappers-for-primitives/task.js
+++ wrappers-for-primitives/task.js
@@ -10,9 +10,20 @@ function calculateMortgage() {
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
-
     // код для задачи №1 писать здесь
     //return totalAmount;
+  "use strict"
+  date = new Date(date);
+  let start = new Date(); // текущее время
+  let n = date - start; // миллисекунды до даты
+  n = Math.floor((n / 3.6e+6 / 24) / (365 / 12)); // месяцев до даты
+  
+  let returnToTheBank = amount - contribution;
+  let P = (percent / 100) / 12;
+  let monthlyPay = returnToTheBank * ( P + P / ( ( (1 + P)**n ) - 1 ) );
+  let totalAmount = monthlyPay * n;
+  console.log(totalAmount);
+  return totalAmount;
 }
 
 function sayHello() {
@@ -25,4 +36,13 @@ function sayHello() {
 function getGreeting(name) {
     // код для задачи №2 писать здесь
     //return greeting;
+    console.log(typeof name);
+    name = name.replace(/^\s+|\s+$/g, '');
+    console.log(name);
+    if (name === '' || name === null || name === undefined) {
+        name = 'Аноним';
+    }; 
+    greeting = `Привет, мир! Меня зовут ${name}`;
+    console.log(greeting);
+    return greeting;
 }
